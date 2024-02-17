fe5ac45d6eacbc30e4c5be3541a9796e4cd97390
SUNIL KUMAR <107296782+sksunil0608@users.noreply.github.com>
2024-02-17 22:14:41 +0530

    Update Home.js

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="h-full w-full">
       <div className="flex flex-col gap-50 px-5 md:px-10  lg:px-60">
         <h1 className=" text-center py-10 italic font-bold text-[30px] text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-          EARNEST FINTECH TASKS
+          Task Management
         </h1>
         <Task />
       </div>
